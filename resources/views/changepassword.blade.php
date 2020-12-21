b87680d917ea2bb72151e039430631815991c498
Chimi <68125101+chimi-pt@users.noreply.github.com>
2020-12-21 09:46:56 +0300

    Update changepassword.blade.php

--- resources/views/changepassword.blade.php
+++ resources/views/changepassword.blade.php
@@ -9,15 +9,15 @@
                 @csrf
                 <div class="form-group">
                 <label for="exampleInputEmail1"><i class="fa fa-lock" aria-hidden="true"></i>Old Password </label>
-                <input type="password" name="password" class="form-control" id="exampleInputEmail1" placeholder="User Name">
+                <input type="password" name="password" class="form-control" id="exampleInputEmail1" placeholder="Old Password">
                 </div>
                 <div class="form-group">
                 <label for="exampleInputEmail1"><i class="fa fa-lock" aria-hidden="true"></i> New Password</label>
-                <input type="password" name="newpassword" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                <input type="password" name="newpassword" class="form-control" id="exampleInputEmail1" placeholder="New Password">
                 </div>
                 <div class="form-group">
                 <label for="exampleInputPassword1"><i class="fa fa-lock" aria-hidden="true"></i>Confirm Password</label>
-                <input type="password" name="confirmpassword" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <input type="password" name="confirmpassword" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password">
                 </div>
                 <button type="submit" name="change" class="btn btn-primary login-button">Change</button>
                 </form>
